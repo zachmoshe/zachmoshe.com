ba87d119687b59e6f35e09d7215dff6d0e69d2db
Zach Moshe <zachmoshe@gmail.com>
2017-05-19 11:34:59 +0300

    adding s3 sync command

--- bin/build_site.sh
+++ bin/build_site.sh
@@ -8,3 +8,13 @@ fi
 
 echo JEKYLL_ENV=$env
 JEKYLL_ENV=$env bundle exec jekyll build
+
+
+if [ "$env" == "development" ]; then
+	aws_command="aws --profile zachmoshe.com s3 sync _site s3://zachmoshe.com-drafts"
+else
+	aws_command="aws --profile zachmoshe.com s3 sync _site s3://zachmoshe.com"
+fi
+
+echo "Run this command to sync with S3:"
+echo $aws_command

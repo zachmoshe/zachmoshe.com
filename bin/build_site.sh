758aded9d34a26711820a656833395c293f7bc18
Zach Moshe <zachmoshe@gmail.com>
2017-11-20 18:50:37 +0200

    change was s3 sync commands to support cache-control

--- bin/build_site.sh
+++ bin/build_site.sh
@@ -14,12 +14,16 @@ fi
 echo JEKYLL_ENV=$env
 JEKYLL_ENV=$env bundle exec jekyll build
 
+# I have no idea how to get rid of this file
+rm -fr _site/assets/.sprockets-manifest-*.json
+
 
 if [ "$env" == "development" ]; then
-	aws_command="aws --profile zachmoshe.com s3 sync _site s3://zachmoshe.com-drafts"
-else
-	aws_command="aws --profile zachmoshe.com s3 sync _site s3://zachmoshe.com"
+	target_bucket="s3://zachmoshe.com-drafts"
+else 
+	target_bucket="s3://zachmoshe.com"
 fi
 
 echo "Run this command to sync with S3:"
-echo $aws_command
+echo "aws --profile zachmoshe.com s3 sync _site $target_bucket --delete --exclude 'assets/*'"
+echo "aws --profile zachmoshe.com s3 sync _site $target_bucket --delete --include 'assets/*' --cache-control max-age=31536000"
